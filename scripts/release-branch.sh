f9fb8efeefd4cec252b75b1e4e46de2f12f1bff5
Daniel Lo Nigro <daniel@dan.cx>
2016-12-14 18:25:43 +1100

    Build RC releases on CircleCI.
    References #2161

--- scripts/release-branch.sh
+++ scripts/release-branch.sh
@@ -2,12 +2,14 @@
 
 set -ex
 
-yarn version --new-version minor
-VERSION=$(node -p -e "require('./package.json').version")
-BRANCH=$(echo "$VERSION" | (IFS="."; read a b c && echo $a.$b-stable))
+OLD_VERSION=$(node -p -e "require('./package.json').version")
+VERSION_BASE=$(echo $OLD_VERSION | ( IFS='.' ; read a b c && echo $a.$((b + 1)) ))
+VERSION="$VERSION_BASE.0-rc.1"
+BRANCH="$VERSION_BASE-stable"
+yarn version --new-version $VERSION
 echo "$BRANCH"
 git checkout -b "$BRANCH"
 git push origin "$BRANCH" --follow-tags
 git checkout master
-yarn version --new-version preminor
+yarn version --new-version preminor --no-git-tag-version
 git push origin master --follow-tags
